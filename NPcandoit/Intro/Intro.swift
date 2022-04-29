3c0561cf871fb370e051c6043dfd53ef6712019b
Seulki Lee <operry126@gmail.com>
2022-04-29 15:12:06 +0900

    인트로화면생성

---
+++ NPcandoit/Intro/Intro.swift
@@ -0,0 +1,19 @@
+//
+//  Intro.swift
+//  NPcandoit
+//
+//  Created by Seulki Lee on 2022/04/29.
+//
+
+import SwiftUI
+
+struct Intro: View {
+        
+    }
+}
+
+struct Intro_Previews: PreviewProvider {
+    static var previews: some View {
+        Intro()
+    }
+}

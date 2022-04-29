92da8f62ae6c508cae00e2eb06b2c9aab7f687a8
Seulki Lee <operry126@gmail.com>
2022-04-29 17:13:57 +0900

    Changed Intro view and Home view

--- NPcandoit/Intro/Intro.swift
+++ NPcandoit/Intro/Intro.swift
@@ -11,12 +11,61 @@ struct Intro: View {
     var body: some View {
         NavigationView{
             TabView {
-                Text("First")
-                Text("Second")
-                Button(action: { }) {
-                    NavigationLink(destination: Text("next page")) {
-                        Text("시작하기").padding()
-                            .frame(width: 300)
+                VStack {
+                    HStack {
+                        Text("N 또는 P여도 문제 없어요!")
+                            .font(.title)
+                            .fontWeight(.bold)
+                        
+                        Spacer()
+                    }
+                    .padding()
+                    
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 10)
+                            .foregroundColor(Color(red: 245/255, green: 255/255, blue: 243/255))
+                        
+                        HStack {
+                            Text("게으름의 천재이신가요? 집중을 통 할 수가 없나요?")
+                                .padding()
+                            
+                            Spacer()
+                        }
+                    }
+                    .padding(20)
+                    .padding(.bottom, 100)
+                    
+                    Spacer()
+                }
+                                
+                HStack {
+                    VStack(alignment: .leading) {
+                        Text("N 또는 P여도 문제 없어요!")
+                            .font(.title)
+                            .fontWeight(.bold)
+                            .padding(.bottom, 50)
+                        
+                        Text("Meet your lovely fairies")
+                        
+                        
+                        Spacer()
+                    }
+                    
+                    Spacer()
+                }
+                .padding()
+                                
+                VStack {
+                    Text("Second")
+                    
+                    Spacer()
+                    
+                    Button(action: { }) {
+                        NavigationLink(destination: Text("next page")) {
+                            Text("시작하기").padding()
+                                .frame(width: 300)
+                        }
+                        .padding(100)
                     }
                 }
             }

f45173c4d771631e1124a757c4848281ff4d3cbe
Seulki Lee <operry126@gmail.com>
2022-05-01 22:06:33 +0900

    created N, P

---
+++ NPcandoit/Nellie/Nellie.swift
@@ -0,0 +1,20 @@
+//
+//  Nellie.swift
+//  NPcandoit
+//
+//  Created by Seulki Lee on 2022/05/01.
+//
+
+import SwiftUI
+
+struct Nellie: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Nellie_Previews: PreviewProvider {
+    static var previews: some View {
+        Nellie()
+    }
+}
---
+++ NPcandoit/Pat/Pat.swift
@@ -0,0 +1,20 @@
+//
+//  Pat.swift
+//  NPcandoit
+//
+//  Created by Seulki Lee on 2022/05/01.
+//
+
+import SwiftUI
+
+struct Pat: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Pat_Previews: PreviewProvider {
+    static var previews: some View {
+        Pat()
+    }
+}
